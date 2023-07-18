8aeb4d0cb57f4dba2b225906d7fbfe9c882b2306
Saeid alebraheem <560739@edu.rocmn.nl>
2023-07-18 16:29:26 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+function nav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+      
+    }
+  }
